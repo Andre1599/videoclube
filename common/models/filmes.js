80fc2c38f736bf2d3ffe4d447711b733f95c27b9
Andre1599 <47611236+Andre1599@users.noreply.github.com>
2019-02-17 20:03:57 +0000

    Add files via upload

---
+++ common/models/filmes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(Filmes) {
+  cosmosdb:
+  {
+    name: "cosmos db",
+    url: "mongodb://grupo:fwKPxl3SiJlXaq2NDvl7KK7iXxtIdmvf0uKHmooc9bTZLUsMNnhkLsvfIQlyujn7eSQfqEq7FJmwv7fWhTyNng==@grupo.documents.azure.com:10255/videoclube?ssl=true",
+    port: 10255
+  }
+};
